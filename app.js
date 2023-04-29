8e01a589173e119ad83047fb9b9f98d8fdf5e565
t-dob-sanghoonlee <t-dob-sanghoonlee@t-dob-sanghoonlee.local>
2023-04-30 08:49:48 +0900

    test

--- app.js
+++ app.js
@@ -28,3 +28,22 @@ module.exports = async function (fastify, opts) {
 }
 
 ////
+const fastify = require('fastify')();
+
+// .env 파일을 로드하기 위해 dotenv 패키지를 설치하고 불러옵니다.
+require('dotenv').config();
+
+fastify.register(require('@fastify/autoload'), {
+  // 연결 정보
+  url: process.env.MONGODB_URI,
+  // 선택 사항: 기본값은 false입니다.
+  // 연결 시도 중에 재시도 할지 여부를 지정합니다.
+  // retryMiliSeconds: 5000 밀리 초 간격으로 재시도합니다.
+  retry: true
+});
+
+fastify.get('/users', async (request, reply) => {
+  const users = await fastify.mongo.db.collection('users').find().toArray();
+  return users;
+});
+
--- plugins/mongodb.js
+++ plugins/mongodb.js
@@ -1,8 +1,12 @@
 'use strict'
 
 const fp = require('fastify-plugin')
+console.log(1234)
+const dotenv = require('dotenv')
+dotenv.config()
 
 const { MONGO_HOSTNAME, MONGO_USERNAME, MONGO_PASSWORD } = process.env
+
 
 module.exports = fp(async function (fastify, opts) {
   const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:27017/?authMechanism=DEFAULT`
