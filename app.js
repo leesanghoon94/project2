09c3b7c7ce8b3b3cf4c02f10e94af8d3182607c4
t-dob-sanghoonlee <t-dob-sanghoonlee@t-dob-sanghoonlee.local>
2023-05-01 18:24:20 +0900

    test

--- app.js
+++ app.js
@@ -47,3 +47,22 @@ fastify.get('/users', async (request, reply) => {
   return users;
 });
 
+const fp = require('fastify-plugin')
+console.log(1234)
+const dotenv = require('dotenv')
+dotenv.config()
+
+const { MONGO_HOSTNAME, MONGO_USERNAME, MONGO_PASSWORD } = process.env
+
+
+module.exports = fp(async function (fastify, opts) {
+  const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}/?authMechanism=DEFAULT`
+  console.log(url)//mongo-nlb-b25c1834de51c921.elb.ap-northeast-2.amazonaws.com, mongodb://lee:sanghoon@mongo-nlb-b25c1834de51c921.elb.ap-northeast-2.amazonaws.com/?authMechanism=DEFAULT
+
+  fastify.register(require('@fastify/mongodb'), {
+    forceClose: true,
+    url: url
+  })
+})
+
+
--- plugins/mongodb.js
+++ plugins/mongodb.js
@@ -1,17 +1,13 @@
 'use strict'
 
 const fp = require('fastify-plugin')
-console.log(1234)
-const dotenv = require('dotenv')
-dotenv.config()
 
 const { MONGO_HOSTNAME, MONGO_USERNAME, MONGO_PASSWORD } = process.env
 
-
 module.exports = fp(async function (fastify, opts) {
-  const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}/?authMechanism=DEFAULT`
-  console.log(url)//mongo-nlb-b25c1834de51c921.elb.ap-northeast-2.amazonaws.com, mongodb://lee:sanghoon@mongo-nlb-b25c1834de51c921.elb.ap-northeast-2.amazonaws.com/?authMechanism=DEFAULT
-
+  const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:27017/?authMechanism=DEFAULT`
+  console.log(url) //mongodb://lee:sanghoon@mongo-nlb-b25c1834de51c921.elb.ap-northeast-2.amazonaws.com:27017/?authMechanism=DEFAULT
+                    //mongodb://lee:sanghoon@mongo-nlb-b25c1834de51c921.elb.ap-northeast-2.amazonaws.com:27017/?authMechanism=DEFAULT
   fastify.register(require('@fastify/mongodb'), {
     forceClose: true,
     url: url
